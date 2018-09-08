7973b7443e3e27bde314f499e82ff5317b767bcf
Shesh Nepal <shesh.nepal@gmail.com>
2018-09-08 19:59:31 -0400

    Hello World!

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+set CONFIGURATION=debug
+set BUILD_DIR=build\%CONFIGURATION%\
+set INTERMEDIATES=%BUILD_DIR%intermediates\
+for %%a in (.) do set CURRENT_FOLDER=%%~nxa
+
+if not exist %INTERMEDIATES% mkdir %INTERMEDIATES%
+
+pushd %BUILD_DIR%
+cl /utf-8 /std:c++latest /DDEBUG /W3 /Zi /MDd /EHsc /Fointermediates\ /nologo ..\..\%CURRENT_FOLDER%\platform\win32\%CURRENT_FOLDER%.win32.cpp kernel32.lib user32.lib /link /OUT:%CURRENT_FOLDER%.exe /INCREMENTAL:NO /NOLOGO
+popd
